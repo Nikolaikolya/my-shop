1734978d83a30893c0d3e359cf0426c3b09597c5
Mikolai Fedotov <starcrafters@ya.ru>
2021-05-16 13:19:12 +0700

    Update server start

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 3035;
 
-app.listen(PORT, () => {
-  console.log(`Server started from ${PORT} port!`);
-});
+const serverStart = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server started from ${PORT} port!`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+serverStart();
